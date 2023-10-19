1611999d11460e025ca3496d48b58b1c0aeb2cc4
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-19 13:14:02 +0000

    first commit

---
+++ datatype-summary.js
@@ -0,0 +1,28 @@
+// sare datatype ke typeof dekhne hai
+var name = "Prafull";
+console.log(typeof name);
+const number = 40;
+console.log(typeof number);
+const students = ["Shivam","Ajay","Pranjal","Priya"];
+
+console.log(typeof students);
+
+const isonn = true;
+console.log("typeof boolean is"+typeof isonn);
+let  cars ;
+
+console.log("type of undefined data type is "+ cars);
+const person = {
+    name:"Yogi baba",
+    branch:"electrical",
+    adress:"Varansi"
+}
+
+console.log(typeof person)
+const myfn =  function(){
+    console.log("hello Objects and Events")
+};
+
+console.log(typeof myfn)
+
+// learning: non primitive data types ka typeof object hota hai
